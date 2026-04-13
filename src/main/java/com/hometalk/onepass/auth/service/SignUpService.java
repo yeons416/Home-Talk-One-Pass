e610386e1890f15777dcebb1b5eeaf74bf90306d
Jaesoon33 <tnswoghkd2638@gmail.com>
2026-04-13 09:55:03 +0900

    [Auth] 회원가입 기능 세분화

--- src/main/java/com/hometalk/onepass/auth/service/SignUpService.java
+++ src/main/java/com/hometalk/onepass/auth/service/SignUpService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class SignUpService {
 
     private final UserRepository userRepository;
     private final LocalAccountRepository localAccountRepository;
